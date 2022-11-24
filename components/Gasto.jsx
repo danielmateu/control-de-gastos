070055f03cff669245ff876bf50f985c112dd5fa
Daniel Mateu <danielmateu86@gmail.com>
2022-11-24 23:35:12 +0100

    Mostrando ICono de los gastos

--- components/Gasto.jsx
+++ components/Gasto.jsx
@@ -1,18 +1,45 @@
+import { formatearFecha } from "../src/helpers";
 
+import IconoAhorro from '../src/img/icono_ahorro.svg'
+import IconoCasa from '../src/img/icono_casa.svg'
+import IconoComida from '../src/img/icono_comida.svg'
+import IconoGastos from '../src/img/icono_gastos.svg'
+import IconoOcio from '../src/img/icono_ocio.svg'
+import IconoSalud from '../src/img/icono_salud.svg'
+import IconoSuscripciones from '../src/img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro : IconoAhorro,
+    casa :  IconoCasa,
+    comida : IconoComida,
+    gastos : IconoGastos,
+    ocio : IconoOcio,
+    salud : IconoSalud,
+    suscripciones : IconoSuscripciones
+}
 
 export const Gasto = ({gasto}) => {
 
-    const {categoria, nombre, cantidad, id} = gasto;
+    const {categoria, nombre, cantidad, id, fecha} = gasto;
 
     return (
         <div className="gasto sombra">
             <div className="contenido-gasto">
+                <img src={diccionarioIconos[categoria]} alt="Icono Gasto" 
+                
+                
+                
+                />
                 <div className="descripcion-gasto">
                     <p className="categoria">{categoria}</p>
-                    <p className="nombre">{nombre}</p>
-                    {/* <p className="cantidad">{cantidad}</p> */}
+                    <p className="nombre-gasto">{nombre}</p>
+                    <p className="fecha-gasto">
+                        Agregado el: {''}
+                        <span> {formatearFecha(fecha)}</span>
+                    </p>
                 </div>
             </div>
+                    <p className="cantidad-gasto">€{cantidad}</p>
         </div>
     )
 }
