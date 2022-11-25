f4a725bf98882c9bd7bbffe8442e34596924e5f6
Daniel Mateu <danielmateu86@gmail.com>
2022-11-25 17:28:14 +0100

    Aplicando efecto de swipe

--- components/Gasto.jsx
+++ components/Gasto.jsx
@@ -1,5 +1,15 @@
 import { formatearFecha } from "../src/helpers";
 
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+} from 'react-swipeable-list';
+
+import 'react-swipeable-list/dist/styles.css';
+
 import IconoAhorro from '../src/img/icono_ahorro.svg'
 import IconoCasa from '../src/img/icono_casa.svg'
 import IconoComida from '../src/img/icono_comida.svg'
@@ -9,34 +19,57 @@ import IconoSalud from '../src/img/icono_salud.svg'
 import IconoSuscripciones from '../src/img/icono_suscripciones.svg'
 
 const diccionarioIconos = {
-    ahorro : IconoAhorro,
-    casa :  IconoCasa,
-    comida : IconoComida,
-    gastos : IconoGastos,
-    ocio : IconoOcio,
-    salud : IconoSalud,
-    suscripciones : IconoSuscripciones
+    ahorro: IconoAhorro,
+    casa: IconoCasa,
+    comida: IconoComida,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones
 }
 
-export const Gasto = ({gasto}) => {
+export const Gasto = ({ gasto }) => {
 
-    const {categoria, nombre, cantidad, id, fecha} = gasto;
+    const { categoria, nombre, cantidad, id, fecha } = gasto;
+
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => console.log('Editar...')}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('Eliminar...')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+
+    )
 
     return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <img src={diccionarioIconos[categoria]} alt="Icono Gasto" 
-                />
-                <div className="descripcion-gasto">
-                    <p className="categoria">{categoria}</p>
-                    <p className="nombre-gasto">{nombre}</p>
-                    <p className="fecha-gasto">
-                        Agregado el: {''}
-                        <span> {formatearFecha(fecha)}</span>
-                    </p>
-                </div>
-            </div>
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions = {leadingActions()}
+                trailingActions = {trailingActions()}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img src={diccionarioIconos[categoria]} alt="Icono Gasto"
+                        />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria}</p>
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado el: {''}
+                                <span> {formatearFecha(fecha)}</span>
+                            </p>
+                        </div>
+                    </div>
                     <p className="cantidad-gasto">€{cantidad}</p>
-        </div>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
